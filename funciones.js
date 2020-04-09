3fd2788b3420691b7a3c5d2d878ac2be1768671a
Diana <dianita.lopez03@gmail.com>
2020-04-08 23:31:15 -0500

    primer commit

---
+++ funciones.js
@@ -0,0 +1,93 @@
+/*function mensaje() {
+    return  'mensaje como argumento de la funcion';
+
+}
+var msj= mensaje();
+console.log(msj);
+*/
+
+/**function sumar (a, b, c=3){
+return a+b+c;
+
+}
+var resultado = sumar(4,5,8);
+var resultado1= sumar (3,7);
+console.log(resultado);
+console.log(resultado1);
+*/
+
+/* FUNCIONES RECURSIVAS*/
+
+/**var factorial = function(n) {
+  if ((n==0) || (n==1))
+ 
+  return 1;
+  else
+  return (n * factorial(n-1));   
+}
+console.log(factorial(5));
+**/
+
+/*Arreglos -Array*/
+
+var nombres= ['diana','carolina','sofia','laura'];
+var vegetales= new Array('tomate','lechuga','zanahoria');
+
+/**console.log(nombres[3]);
+console.log(vegetales[1]);
+
+nombres[0]= 'jose';
+vegetales[2]='cebolla';
+
+console.log(nombres[0]);
+console.log(vegetales[2]);*/
+
+// console.log(nombres.length);
+// for (var i=0; i<=nombres.length-1; i++){
+// console.log(nombres[i]);
+// }
+// vegetales.forEach(function(elemento, indice){
+//   console.log(elemento, indice)
+// }
+// )
+
+/**console.log(nombres);
+nombres.push('monica');
+console.log(nombres);
+nombres.unshift('Zuleyda');
+console.log(nombres);
+nombres.pop();
+console.log(nombres);
+nombres.shift();
+console.log(nombres);
+
+var pos= nombres.indexOf('carolina');
+console.log(pos);
+
+nombres.splice(1,2);
+console.log(nombres);**/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
